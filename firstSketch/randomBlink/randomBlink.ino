90f174add301a66ca21c01a469c926786a8d4877
Michael Pardi <michael@michaels-mbp.mobile.asu.edu>
2017-08-23 10:02:38 -0700

    Added randomBlink sketch, made blink for 5 seconds.

--- firstSketch/randomBlink/randomBlink.ino
+++ firstSketch/randomBlink/randomBlink.ino
@@ -20,19 +20,27 @@
   by Colby Newman
 */
 
+const int LED_CHOICE = 2;
+
+long interval;
+
 
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LED_CHOICE, OUTPUT);
+  randomSeed(analogRead(0));
 }
 
 // the loop function runs over and over again forever
 void loop() {
 
-  //The writer is interted, so LOW is on, and HIGH is off.
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(5000);                       // wait for a second
+  interval = random(100, 1000);
+  
+  digitalWrite(LED_CHOICE, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(interval);                       // wait for a second
+  
+  digitalWrite(LED_CHOICE, LOW);    // turn the LED off by making the voltage LOW
+  
+  delay(250);                       // wait for a second
 }
