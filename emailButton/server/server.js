026d85d56af3611b57f114dc83605623f6330901
Michael Pardi <michael@michaels-mbp.mobile.asu.edu>
2017-09-27 10:28:01 -0700

    emailButton modified

--- emailButton/server/server.js
+++ emailButton/server/server.js
@@ -16,8 +16,9 @@ nodemailer.createTestAccount((err, account) => {
 
     // create reusable transporter object using the default SMTP transport
     let transporter = nodemailer.createTransport({
-        host: 'smtp.gmail.email',
-        port: 587,
+//        host: 'smtp.gmail.email',
+//        port: 587,
+        service: "Gmail",
         secure: false, // true for 465, false for other ports
         auth: {
             user: "michaelthomaspardi@gmail.com", // generated ethereal user
