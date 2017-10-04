b70d55db2fbce92d0059bcd5d98e5d4b164a876c
MPardi <MPardi@asu.edu>
2017-10-04 17:01:08 +0000

    Added temp/light project

---
+++ nonPDataLogging/server/server.js
@@ -0,0 +1,21 @@
+var express = require("express");
+var app = express();
+var bodyParser = require('body-parser');
+var errorHandler = require('errorhandler');
+var methodOverride = require('method-override');
+var hostname = process.env.HOSTNAME || 'localhost';
+var port = 8080;
+
+app.get("/update", function (req, res) {
+    console.log("REQUEST RECEIVED... Request = ")
+	console.log(req.query)
+        res.send("Server successfully implemented...")
+});
+
+app.use(methodOverride());
+app.use(bodyParser());
+//app.use(express.static(__dirname + '/public'));
+app.use(errorHandler());
+
+console.log("Simple static server listening at http://" + hostname + ":" + port);
+app.listen(port);
